749677f05b43f2240dce190324c07463685f284b
jrnold <jeffrey.arnold@gmail.com>
2017-09-12 17:40:03 -0700

    misc updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='textstuff',
-      version='0.0.1',
+      version='0.0.2',
       description='Miscellaneous functions for dealing with text data',
       url='http://github.com/jrnold/textstuff',
       author='Jeffrey B. Arnold',
--- textstuff/utils.py
+++ textstuff/utils.py
@@ -1,5 +1,11 @@
+""" Miscellaneous utility functions """
+import re
+from itertools import chain
+
 import email_normalize
 import tldextract
+
+_CAMEL_CASE_PATTERN = r".+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)"
 
 
 def normalize_email(email):
@@ -36,3 +42,44 @@ def normalize_url(url):
 
     """
     return '.'.join(tldextract.extract(url)[1:])
+
+
+# def lik_phone_number(string):
+#     """ String is like a phone number
+#
+#     Parameters
+#     -----------
+#     string: str
+#         A string
+#
+#     Returns
+#     --------
+#     bool
+#         ``True`` is the string is a phone number.
+#
+#     """
+#     textacy.
+
+
+def split_camel_case(string):
+    """ Split Word on Camel Case
+
+    Split word on internal uppercase letters, but allowing for sequences of
+    all-caps letters.
+
+    Parameters
+    -----------
+    string: str
+        The string to split
+
+    Returns
+    --------
+    list of str
+        A list of strings after splitting the word
+
+    This pattern is from this
+    `stackoverflow <https://stackoverflow.com/questions/29916065/how-to-do-camelcase-split-in-python>`__.
+    answer.
+
+    """  # noqc
+    return [m.group(0) for m in re.finditer(_CAMEL_CASE_PATTERN, string)]
