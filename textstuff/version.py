08bcff6b5dd2c73209c1291405286b370b449ad5
jrnold <jeffrey.arnold@gmail.com>
2017-09-23 23:16:18 -0700

    add corpus utility functions

--- textstuff/version.py
+++ textstuff/version.py
@@ -2,4 +2,4 @@
 
 # See https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package  # noqa
 # for reasons to define the version in a separate file.  # noqa
-__version__ = "0.7.0"
+__version__ = "0.8.0"
